a7d9537342e2aa47e2234546bd0c2cc0bb248df4
as-kashmary <askashmary@gmail.com>
2025-11-01 07:10:30 +0600

    code updated and symptoms csv created

--- dataset/translate.py
+++ dataset/translate.py
@@ -1,3 +1,10 @@
 import pandas as pd
 
-df=pd.read_csv('data/dataset_tabular.csv')
+df=pd.read_csv('dataset\dataset_tabular.csv')
+symptom_columns = df.columns[1:].tolist()  # Exclude the first column (disease names)
+df_symptoms = pd.DataFrame({'symptom': symptom_columns})
+
+output_file = 'dataset/symptoms.csv'
+df_symptoms.to_csv(output_file, index=False)
+
+print(f"Successfully created '{output_file}' with symptom column headers.")
